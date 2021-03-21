36439f41bb12103dafd1b463c95aba048de34d95
neo <neo@neo>
2021-03-21 11:34:59 -0400

    finished making the trivia app. The last part I did was the js part

---
+++ main.js
@@ -0,0 +1,112 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is my favorite color?',
+    answers: [
+      { text: 'Blue', correct: true },
+      { text: 'Green', correct: false }
+    ]
+  },
+  {
+    question: 'Who is my all time favorite soccer player?',
+    answers: [
+      { text: 'Fabregas', correct: false },
+      { text: 'Sanches', correct: false },
+      { text: 'Henry', correct: true },
+      { text: 'Messi', correct: false }
+    ]
+  },
+  {
+    question: 'Best team in the premier league...',
+    answers: [
+      { text: 'Chelsea', correct: false },
+      { text: 'Arsenal', correct: true },
+      { text: 'Man Utd', correct: false },
+      { text: 'Man City', correct: false }
+    ]
+  },
+  {
+    question: 'What is my favorite operating system',
+    answers: [
+      { text: 'Windows', correct: false },
+      { text: 'Linux', correct: true }
+    ]
+  }
+]
